ee79c97f4994d8f4a13da6b04e416ad09e856f1d
A1an-org <Alanpshtiwansadq@gmail.com>
2021-11-09 07:23:49 +0300

    Working

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { Embed } from '@discordjs/builders';
+import { error } from 'console';
+import Discord, { 
+    Intents,
+} from 'discord.js'
+import path from 'path'
+import WOKCommands from 'wokcommands'
+
+const client = new Discord.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+
+client.on('ready', () => {
+    console.log("Bot is online!");
+
+    new WOKCommands(client, {
+        commandsDir: path.join(__dirname, 'commands'),
+        typeScript: true
+    })
+})
+
+
+
+
+client.login("NzMzNzU1MzAwNzk2MTcwMzMy.XxHw4A.qPkZ7w7FMbs3mAq1Kwg2zo_sDXU");
